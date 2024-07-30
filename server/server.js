21e1a9b24072c158b7406cd5ca92cfcd749ee35b
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-30 16:57:48 -0700

    Started....

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+const mongoUri = `mongodb+srv://aahmad19376:${DB_PASS}@${DB_HOST}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(mongoUri)
+.then(()=>{
+    console.log('Mongo Connected....')
+})
+.catch((err)=>{
+    console.log("Mongo not connected...", err);
+})
+
+
+// PARSING
+app.use(bodyParser.json())
+
+
+// SANITIZE
+
+app.use(xss());
+
+app.use(mongoSanitize());
+
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+});
